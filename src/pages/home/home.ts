dc3e9699550291b2fa957bff31cc8339376f29bf
김형태 <gudxo1226@naver.com>
2022-11-04 13:53:08 +0900

    no message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -32,4 +32,19 @@ export class HomePage {
   slideNext() {
     this.slides.slideNext();
   }
+
+  allowDrop(ev) {
+    ev.preventDefault();
+  }
+
+  drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+
+  drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+  
 }
