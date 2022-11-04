e79e9dd81392d2ff4e392abba4502cb2f9791bcf
김형태 <gudxo1226@naver.com>
2022-11-04 17:14:26 +0900

    드래그앤 드롭 로직 추가.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,65 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import * as firebase from "firebase"
-import { MyApp } from './app.component';
-// import { LoginPage } from '../pages/login/login';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { OnoffutilProvider } from '../providers/onoffutil/onoffutil';
-import { HttpClient, HttpHandler, HttpClientModule } from '@angular/common/http';
-import { ScreenOrientation } from '@ionic-native/screen-orientation/';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
-import { FCM } from '@ionic-native/fcm/ngx';
-import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
-import { ImagePicker } from '@ionic-native/image-picker/ngx';
-import { DrawingPage } from '../pages/drawing/drawing';
-import { HomePage } from '../pages/home/home';
-import { EmailComposer } from '@ionic-native/email-composer/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+// import * as firebase from "firebase"
+import { DragulaModule } from 'ng2-dragula';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDnAhTYDrVx_1hqwChoD44C5SbCl97soGA",
-  authDomain: "forch-74f10.firebaseapp.com",
-  databaseURL: "https://forch-74f10-default-rtdb.firebaseio.com",
-  projectId: "forch-74f10",
-  storageBucket: "forch-74f10.appspot.com",
-  messagingSenderId: "164203318212",
-  appId: "1:164203318212:web:1d553bebd574632a92b291",
-  measurementId: "G-46Y6GB6FHV"
-};
-firebase.initializeApp(firebaseConfig);
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    DrawingPage,
-    HomePage,
+    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    AngularFireDatabaseModule,
-    HttpClientModule,
-    AngularFireModule.initializeApp(firebaseConfig),
+    DragulaModule.forRoot(),
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    DrawingPage,
-    HomePage,
+    HomePage
   ],
   providers: [
-    AngularFireAuth,
-    Camera,
-    PhotoViewer,
-    Geolocation,
-    FCM,
-    ImagePicker,
-    EmailComposer,
-    { provide: ErrorHandler, useClass: IonicErrorHandler },
-    OnoffutilProvider,
+    // StatusBar,
+    // SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule { }
+export class AppModule {}
