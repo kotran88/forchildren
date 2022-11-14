a23e282a781147c94516393a5e1d370d90f2902d
milpy <jjh08044@gmail.com>
2022-11-14 10:55:53 +0900

    app.component.ts, app.module.ts add login2, drawing

--- src/pages/login2/login2.ts
+++ src/pages/login2/login2.ts
@@ -8,7 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-login2',
   templateUrl: 'login2.html',
